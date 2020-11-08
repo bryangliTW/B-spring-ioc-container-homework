f58acff63336a99af2383005611079a8332cbffb
Bryan Li <yang.li@thoughtworks.com>
2020-11-08 18:48:40 +0800

    completed the second solution

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +10,18 @@ import static org.springframework.beans.factory.config.ConfigurableBeanFactory.S
 
 @RestController
 @Scope(SCOPE_SINGLETON)
-public class GreetingController implements ApplicationContextAware {
+public class GreetingController {
 
-    private ApplicationContext applicationContext;
+    private final GreetingService greetingService;
 
-    public GreetingService getGreetingService() {
-        return applicationContext.getBean(GreetingService.class);
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+    @Lookup
+    public GreetingService getGreetingService() {
+        return null;
     }
 
     @GetMapping("/greet")
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -8,6 +8,7 @@ import static org.springframework.beans.factory.config.ConfigurableBeanFactory.S
 @Service
 @Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
+
     String sayHi() {
         return "hello world";
     }
