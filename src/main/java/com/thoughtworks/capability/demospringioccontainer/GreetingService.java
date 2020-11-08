3a9ce56d8c8d1a0332ed76cc7c83bee64a0e24eb
Bryan Li <yang.li@thoughtworks.com>
2020-11-08 18:43:25 +0800

    completed solution 1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service
-public class GreetingService {
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
+@Service
+@Scope(SCOPE_PROTOTYPE)
+public class GreetingService {
     String sayHi() {
         return "hello world";
     }
